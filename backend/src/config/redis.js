a569f89512c32d1bf2e4b2715a07c9dc41e7dcd2
Yadnyesh More <yadnyeshmore@gmail.com>
2025-12-28 15:02:33 +0530

    Now Deploying without Docker The changes will Comming Soon ...

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,8 +1,9 @@
 import redis from 'redis';
 
+const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
+
 const client = redis.createClient({
-  host: process.env.REDIS_HOST || 'localhost',
-  port: process.env.REDIS_PORT || 6379,
+  url: redisUrl,
 });
 
 client.on('error', (err) => {
