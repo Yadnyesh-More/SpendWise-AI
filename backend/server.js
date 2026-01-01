c8ca3cf8a53174475616f478daeb4fa0e6d675a4
Yadnyesh More <yadnyeshmore@gmail.com>
2026-01-01 15:41:31 +0530

    Import Path Change of rmote.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ import adminRoutes from './src/routes/adminRoutes.js';
 import aiRoutes from './src/routes/aiRoutes.js';
 import authMiddleware from './src/middleware/auth.js';
 import analyticsRoutes from './src/routes/analytics.js';
-import exportRoutes from './routes/export.js';
+import exportRoutes from './src/routes/export.js';
 import fraudDetection from './middleware/fraudDetection.js';
 
 dotenv.config();
